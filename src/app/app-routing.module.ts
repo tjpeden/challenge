728bca40798a44246e8feb5f136dc2af8d16950e
TJ Peden <tj@tjcoding.com>
2020-09-20 16:24:55 -0500

    Main Layout
    - Added a layout for the logged in portion of the portal
    - Used some flexbox and some CSS Grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './pages/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutComponent } from './pages/layout/layout.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+
+    MatToolbarModule,
+
     AppRoutingModule,
   ],
   providers: [],
