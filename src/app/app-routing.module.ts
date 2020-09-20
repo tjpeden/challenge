96e0f99f9858c2f42724c395548a441a662f5101
TJ Peden <tj@tjcoding.com>
2020-09-20 17:14:32 -0500

    Landing Page Placeholder
    - Add a simple landing page
    - Switched to custom theme for SCSS variable support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [
+      { path: '', pathMatch: 'full', component: HomeComponent },
     ],
   },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
@@ -10,18 +12,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
     LayoutComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
 
+    MatCardModule,
+    MatButtonModule,
     MatToolbarModule,
 
     AppRoutingModule,
