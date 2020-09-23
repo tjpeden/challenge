e0f91ea8f4e2bab39fbef62ce8bd24284139cab7
TJ Peden <tj@tjcoding.com>
2020-09-23 15:04:38 -0500

    Final clean up

--- src/app/application/pages/application-instance/application-instance.component.ts
+++ src/app/application/pages/application-instance/application-instance.component.ts
@@ -1,11 +1,11 @@
 import {
   Component,
-  OnDestroy,
   OnInit,
 } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import {
@@ -18,7 +18,10 @@ import {
   MatTreeFlatDataSource,
 } from '@angular/material/tree';
 
-import { NgFormsManager } from '@ngneat/forms-manager';
+import {
+  untilDestroyed,
+  UntilDestroy,
+} from '@ngneat/until-destroy'
 
 import {
   BehaviorSubject,
@@ -28,22 +31,20 @@ import {
 import {
   map as rxMap,
   switchMap,
-  takeUntil,
-  tap as rxTap,
 } from 'rxjs/operators'
 
 import {
   head,
-  map,
   pipe,
   prop,
   propOr,
   find,
   whereEq,
   chain,
+  reduce,
 } from 'ramda';
 
-import emailMask from 'text-mask-addons/dist/emailMask'
+import emailMask from 'text-mask-addons/dist/emailMask';
 
 import {
   Application,
@@ -58,13 +59,14 @@ interface FlatNode {
   expandable: boolean
 }
 
+@UntilDestroy()
 @Component({
   selector: 'app-application-instance',
   templateUrl: './application-instance.component.html',
   styleUrls: ['./application-instance.component.scss']
 })
-export class ApplicationInstanceComponent implements OnInit, OnDestroy {
-  private destroy: Subject<void>
+export class ApplicationInstanceComponent implements OnInit {
+  public form: FormGroup
 
   public subFormId: Subject<string>
   public application: BehaviorSubject<Application>
@@ -82,12 +84,10 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
   }
 
   public constructor(
-    private manager: NgFormsManager,
     private builder: FormBuilder,
     private service: ApplicationService,
     private route: ActivatedRoute,
   ) {
-    this.destroy = new Subject()
     this.subFormId = new Subject()
     this.application = new BehaviorSubject(null)
     this.sections = new BehaviorSubject([])
@@ -111,17 +111,16 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     const application$ = this.route.params
     .pipe(
+      untilDestroyed(this),
       rxMap(prop('id')),
       switchMap((applicationInstanceId: string) => this.service.fetch({ applicationInstanceId })),
-      takeUntil(this.destroy),
     )
 
     application$.subscribe(this.application)
 
     const tree$ = application$
     .pipe(
-      rxMap(propOr([], 'children')),
-      takeUntil(this.destroy),
+      rxMap<Application, ApplicationBits[]>(propOr([], 'children')),
     )
 
     tree$.subscribe(
@@ -132,7 +131,19 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
       }
     )
 
-    const sections$ = combineLatest(
+    tree$
+    .pipe(
+      rxMap(
+        pipe<readonly ApplicationBits[], ApplicationBits[], ApplicationBits[], FormGroup>(
+          chain(propOr([], 'children')),
+          chain(propOr([], 'children')),
+          (sections: ApplicationBits[]) => this.createForm(sections),
+        ),
+      ),
+    )
+    .subscribe(form => this.form = form)
+
+    combineLatest(
       tree$,
       this.subFormId,
       (tree, id) => {
@@ -147,16 +158,7 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
     .pipe(
       rxMap(prop('children')),
     )
-
-    sections$
-    .pipe(
-      rxMap(
-        map((section: ApplicationBits) => this.createForm(section))
-      )
-    )
-    .subscribe(this.formGroups)
-
-    sections$.subscribe(this.sections)
+    .subscribe(this.sections)
 
     tree$
     .pipe(
@@ -170,12 +172,6 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
       ),
     )
     .subscribe((id: string) => this.setSubForm(id))
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy.next()
-    this.destroy.unsubscribe()
-    this.manager.unsubscribe()
   }
 
   public hasChild(_: number, node: FlatNode): boolean {
@@ -193,8 +189,16 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
     return `${type}${id}`
   }
 
-  public getField(name: string, index: number) {
-    return this.manager.getControl(name, `fields.${index}`)
+  public getForm(section: string): FormGroup {
+    return this.form.get(section) as FormGroup
+  }
+
+  public getField(section: string, field: string) {
+    return this.form.get([section, field])
+  }
+
+  public submit() {
+    this.form.disable()
   }
 
   private transformer({ id, type, title: name, children }: ApplicationBits, level: number): FlatNode {
@@ -206,82 +210,106 @@ export class ApplicationInstanceComponent implements OnInit, OnDestroy {
     }
   }
 
-  private createForm(section: ApplicationBits) {
-    const name = this.getName(section)
+  // This needs to be refactored to be more DRY
+  private createForm(sections: ApplicationBits[]): FormGroup {
+    const controlsConfig = reduce(
+      (group, section) => {
+        const name = this.getName(section)
 
-    const controlsConfig = section.children.map(
-      field => {
-        const validators = []
+        group[name] = this.createSectionFormGroup(section)
 
-        if (field.required) {
-          validators.push(Validators.required)
-        }
-
-        switch (field.format) {
-          case 'email': {
-            validators.push(Validators.email)
-
-            break
-          }
-          case 'phonenumber': {
-            validators.push(Validators.pattern(/^\(\d{3}\) \d{3}-\d{4}$/))
-
-            break
-          }
-          case 'ssn': {
-            validators.push(Validators.pattern(/^\d{3}-\d{2}-\d{4}$/))
-
-            break
-          }
-          case 'date': {
-            validators.push(Validators.pattern(/^\d{4}-\d{2}-\d{2}$/))
-
-            break
-          }
-          case 'zipCode': {
-            validators.push(Validators.pattern(/^\d{5}$/))
-
-            break
-          }
-          case 'zipCode+4': {
-            validators.push(Validators.pattern(/^\d{5}-\d{4}$/))
-
-            break
-          }
-          case 'alphanumeric': {
-            validators.push(Validators.pattern(/^[0-9A-Za-z]*$/))
-
-            break
-          }
-          case 'onlyLetters': {
-            validators.push(Validators.pattern(/^[A-Za-z]*$/))
-
-            break
-          }
-          case 'onlyNumbers': {
-            validators.push(Validators.pattern(/^[0-9]*$/))
-
-            break
-          }
-          // Running low on time. Very crude implementation here.
-          case 'currency': {
-            validators.push(Validators.pattern(/^\$\d+\.\d{2}$/))
-
-            break
-          }
-          default: break
-        }
-
-        return this.builder.control(null, Validators.compose(validators))
-      }
+        return group
+      },
+      {},
+      sections,
     )
-    const formGroup = this.builder.group({
-      fields: this.builder.array(controlsConfig),
-    })
 
-    this.manager.upsert(name, formGroup)
+    return this.builder.group(controlsConfig)
+  }
 
-    return formGroup
+  private createSectionFormGroup(section: ApplicationBits): FormGroup {
+    const controlsConfig = reduce(
+      (group, field) => {
+        const name = this.getName(field)
+
+        group[name] = [null, this.getValidators(field)]
+
+        return group
+      },
+      {},
+      section.children,
+    )
+
+    return this.builder.group(controlsConfig)
+  }
+
+  private getValidators(field: ApplicationBits): ValidatorFn {
+    const validators = []
+
+    switch (field.format) {
+      case 'email': {
+        validators.push(Validators.email)
+
+        break
+      }
+      case 'phonenumber': {
+        validators.push(Validators.pattern(/^\(\d{3}\) \d{3}-\d{4}$/))
+
+        break
+      }
+      case 'ssn': {
+        validators.push(Validators.pattern(/^\d{3}-\d{2}-\d{4}$/))
+
+        break
+      }
+      case 'date': {
+        validators.push(Validators.pattern(/^\d{4}-\d{2}-\d{2}$/))
+
+        break
+      }
+      case 'zipCode': {
+        validators.push(Validators.pattern(/^\d{5}$/))
+
+        break
+      }
+      case 'zipCode+4': {
+        validators.push(Validators.pattern(/^\d{5}-\d{4}$/))
+
+        break
+      }
+      case 'alphanumeric': {
+        validators.push(Validators.pattern(/^[0-9A-Za-z]*$/))
+
+        break
+      }
+      case 'onlyLetters': {
+        validators.push(Validators.pattern(/^[A-Za-z]*$/))
+
+        break
+      }
+      case 'onlyNumbers': {
+        validators.push(Validators.pattern(/^[0-9]*$/))
+
+        break
+      }
+      // Running low on time. Very crude implementation here.
+      case 'currency': {
+        validators.push(Validators.pattern(/^\$\d+\.\d{2}$/))
+
+        break
+      }
+      default: break
+    }
+
+    if (field.required) {
+      if (validators.length === 0) {
+        return Validators.required
+      }
+
+      validators.push(Validators.required)
+    }
+
+    return Validators.compose(validators)
   }
 
 }
